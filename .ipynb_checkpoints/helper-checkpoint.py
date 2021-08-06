cc19c512e24915ba48c8e93a708450fc5f16cb02
Carlos Selmo <cselmo@itba.edu.ar>
2021-08-06 20:33:51 -0300

    muestra para alumnos de RN

---
+++ .ipynb_checkpoints/helper-checkpoint.py
@@ -0,0 +1,16 @@
+def suma(a,b):
+    return a+b
+
+class ClaseMath:
+    def __init__(self, a, b=5):
+        self.a = a
+        self.b = b
+        print("Clase inicializada")
+    def suma(self):
+        return self.a + self.b
+    def resta(self):
+        return self.a - self.b
+    def __multip(self):
+        return a*b
+    def mult(self):
+        return self.__multip()
